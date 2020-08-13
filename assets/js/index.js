d77d27ca44b0a5b619ab6d94c665265e7743119a
marchen <1002833252@qq.com>
2020-08-13 15:10:18 +0800

    完成首页

---
+++ assets/js/index.js
@@ -0,0 +1,51 @@
+$(function () {
+    // 获取用户基本信息
+    getUserInfo()
+    var layer = layui.layer
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function () {
+        // console.log(11);
+        layer.confirm('确定退出登录？', {
+            btn: ['确定', '取消'] //按钮
+        }, function (index) {
+            // 1.清空本地存储中的token
+            localStorage.removeItem("token")
+            // 2.重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭confirm询问框
+            layer.close(index)
+        })
+    })
+
+    function getUserInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layui.layer.msg('获取用户信息失败！')
+                }
+                renderAvatar(res.data)
+            }
+        })
+    }
+    // 渲染用户头像
+    function renderAvatar(user) {
+        // 获取用户名称
+        var name = user.nickname || user.username
+        // 设置欢迎的文本
+        $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+        if (user.user_pic !== null) {
+            // 渲染图片头像
+            $(".layui-nav-img").attr('src', user.user_pic).show()
+            $(".text-avatar").hide()
+        } else {
+            // 渲染文字头像
+            $(".layui-nav-img").hide()
+            var first = name[0].toUpperCase()
+            $(".text-avatar").html(first).show()
+        }
+    }
+
+})
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,49 +1,51 @@
 $(function () {
-    // 点击注册按钮跳转页面
+    // 点击注册和登录按钮跳转页面
     $("#link_reg").on('click', function () {
         $(".login-box").hide()
         $(".reg-box").show()
     })
-    // 点击登录按钮跳转页面
     $("#link_login").on('click', function () {
         $('.reg-box').hide()
         $('.login-box').show()
     })
-    // 给表单添加啊验证规则
+    // 给表单添加验证规则
     var form = layui.form
-    var layer = layui.layer
     form.verify({
         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
         repwd: function (value) {
             var pwd = $(".reg-box [name=password]").val()
             if (pwd !== value) {
-                return "两次密码不一致"
+                return "两次密码不一致！"
             }
         }
     })
     // 监听注册表单的提交事件
-    $("#form_reg").on("submit", function (e) {
-        // 阻止默认提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 阻止表单默认提交事件
         e.preventDefault()
         // 发起ajax请求
         var data = {
             username: $('#form_reg [name=username]').val(),
             password: $('#form_reg [name=password]').val()
         }
-        $.post('/api/reguser', data, function (res) {
-            if (res.status !== 0) {
-                return layer.msg(res.message)
+        $.post({
+            url: '/api/reguser',
+            data: data,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message)
+                }
+                layer.msg('注册成功！')
+                $("#link_login").click()
             }
-            layer.msg('注册成功，请登录！')
-            $("#link_login").click()
         })
     })
     // 监听登录表单的提交事件
-    $("#form_login").on("submit", function (e) {
+    $("#form_login").on('submit', function (e) {
         e.preventDefault()
         $.ajax({
-            url: '/api/login',
-            method: 'post',
+            method: "post",
+            url: "/api/login",
             data: $(this).serialize(),
             success: function (res) {
                 if (res.status !== 0) {
@@ -56,3 +58,67 @@ $(function () {
         })
     })
 })
+
+
+
+
+
+
+// $(function () {
+//     // 点击注册按钮跳转页面
+//     $("#link_reg").on('click', function () {
+//         $(".login-box").hide()
+//         $(".reg-box").show()
+//     })
+//     // 点击登录按钮跳转页面
+//     $("#link_login").on('click', function () {
+//         $('.reg-box').hide()
+//         $('.login-box').show()
+//     })
+//     // 给表单添加啊验证规则
+//     var form = layui.form
+//     var layer = layui.layer
+//     form.verify({
+//         pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+//         repwd: function (value) {
+//             var pwd = $(".reg-box [name=password]").val()
+//             if (pwd !== value) {
+//                 return "两次密码不一致"
+//             }
+//         }
+//     })
+//     // 监听注册表单的提交事件
+//     $("#form_reg").on("submit", function (e) {
+//         // 阻止默认提交事件
+//         e.preventDefault()
+//         // 发起ajax请求
+//         var data = {
+//             username: $('#form_reg [name=username]').val(),
+//             password: $('#form_reg [name=password]').val()
+//         }
+//         $.post('/api/reguser', data, function (res) {
+//             if (res.status !== 0) {
+//                 return layer.msg(res.message)
+//             }
+//             layer.msg('注册成功，请登录！')
+//             $("#link_login").click()
+//         })
+//     })
+//     // 监听登录表单的提交事件
+//     $("#form_login").on("submit", function (e) {
+//         e.preventDefault()
+//         $.ajax({
+//             url: '/api/login',
+//             method: 'post',
+//             data: $(this).serialize(),
+//             success: function (res) {
+//                 if (res.status !== 0) {
+//                     return layer.msg('登录失败！')
+//                 }
+//                 layer.msg('登录成功！')
+//                 localStorage.setItem('token', res.token)
+//                 location.href = '/index.html'
+//             }
+//         })
+//     })
+// })
